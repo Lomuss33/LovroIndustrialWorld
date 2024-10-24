794489f720bb389351dc4d7852c93a6fc799e844
Lomuss33 <lovro.music.3@gmail.com>
2024-10-24 22:03:06 +0200

    Updated script.js with LF/CRLF changes

--- src/script.js
+++ src/script.js
@@ -1,61 +1,73 @@
+import './style.css'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
+/**
+ * Base
+ */
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
+
+// Sizes
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+// Cursor
+const cursor = {
+    x: 0,
+    y: 0
+}
+
+window.addEventListener('mousemove', (event) =>
+{
+    cursor.x = event.clientX / sizes.width - 0.5
+    cursor.y = - (event.clientY / sizes.height - 0.5)
+})
 
 // Scene
 const scene = new THREE.Scene()
 
-/**
- * Object
- */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-mesh.position.x = 0.5
-mesh.position.y = 1.5
-mesh.position.z = 1
-
-mesh.position.set(0.7, 0.6, -2 )
-
+// Object
+const mesh = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
 scene.add(mesh)
 
-
-// Scale
-mesh.scale.set(4, 2, 13)
-
-// Axes helper
-const axesHelper = new THREE.AxesHelper(4)
-scene.add(axesHelper)
-axesHelper.position.set(1, -1, 5)
-
-/**
- * Sizes
- */
-const sizes = {
-    width: 800,
-    height: 600
-}
-
-/**
- * Camera
- */
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.set(0, 2, 15)
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+// const aspectRatio = sizes.width / sizes.height
+// const camera = new THREE.OrthographicCamera(- 1 * aspectRatio, 1 * aspectRatio, 1, - 1, 0.1, 100)
+camera.position.z = 3
 scene.add(camera)
 
-/**
- * Renderer
- */
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+// Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
 
-console.log("length mesh: position from centar " + mesh.position.length())
-console.log("positon to mesh: " + mesh.position.distanceTo(new THREE.Vector3(0, 1, 2)))
-console.log("object to camera position: " + mesh.position.distanceTo(camera.position))
-console.log(mesh.position.length())
-mesh.position.normalize() // position object on length (x) 1
-console.log(mesh.position.length())
+// Animate
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
